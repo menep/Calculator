303b42b2675cdc60738171f97c95921dacc5be20
Pierantonio <menep.dev@gmail.com>
2018-07-02 19:16:13 +0200

    v04, calculator is operational, all planned stages have been implemented

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Calculator
+A Javascript calculator
+
+Only one JS file, structured upon a module pattern (data controller, ui controller, general controller)
+
+Current stage:
+
+==================== v04 (final) ====================
+
+Implementation of C and CE buttons
+
+=====================================================
+
+
+----------- Past stages -----------
+
+v01) Calculator with basic functionality:
+
+the user can perform one operation at a time (sum, subtraction, multiplication, division);
+
+v02) More operations can be done in a row
+
+v03) Implementation of the DOT button
 
--- source/resources/script.js
+++ source/resources/script.js
@@ -1,59 +1,113 @@
 const dataController = (function() {
-    let numberSwitcher = true, // to switch between tempNumberOne and tempNumberTwo
-        tempNumberOne = "",
-        tempNumberTwo = "",
-        tempResult = 0,
-        operatorSwitcher = true,
-        operator = "";
+    let curInput = [],
+        inputStorage = [],
+        operatorStorage = [],
+        regex = /[\d.]/;
 
-    function storeInput(dataset) {
-        numberSwitcher
-            ? (tempNumberOne += dataset)
-            : (tempNumberTwo += dataset);
+    function storeCurInput(dset) {
+        if (!regex.test(dset)) {
+            // if input is not a digit nor a dot -> therefore an operator or equals
+            if (curInput.length === 0 && inputStorage.length === 0) {
+                // if no number was provided before the operator, the first term of the operation is missing ->
+                return;
+            } else {
+                // push the input number to the storage and clear it
+                inputStorage.push(curInput.join(""));
+                curInput = [];
+                if (inputStorage[inputStorage.length - 1] === "") {
+                    // in the previous passage, if two operators are clicked in a row, an empty value is pushed onto inputStorage -> we remove it, and just update the last value of operatorStorage without adding a new one
+                    if (operatorStorage.length === 0) {
+                        operatorStorage.push(dset);
+                        inputStorage.pop();
+                    } else {
+                        operatorStorage[operatorStorage.length - 1] = dset;
+                        inputStorage.pop();
+                    }
+                } else {
+                    // if instead it's the first operator to be inputted after a number, we just add it to operatorStorage
+
+                    operatorStorage.push(dset);
+                }
+            }
+        } else {
+            // if input is a digit or a dot
+            if (dset === ".") {
+                // if last input is a dot or there's already a dot in the current number, just return
+                if (
+                    curInput[curInput.length - 1] === "." ||
+                    curInput.indexOf(dset) !== -1
+                ) {
+                    return;
+                } else {
+                    curInput.push(dset);
+                }
+            } else {
+                if (inputStorage.length > operatorStorage.length) {
+                    // this is to reset the calculations after the operations have run, when the first input after a "=" sign is a number
+                    curInput = [];
+                    inputStorage = [];
+                    operatorStorage = [];
+                    curInput.push(dset);
+                } else {
+                    // if input is a number
+                    curInput.push(dset);
+                }
+            }
+        }
+        console.log(inputStorage, operatorStorage);
     }
 
-    function resetNumbers() {
-        tempNumberOne = "";
-        tempNumberTwo = "";
+    function operations() {
+        // we add the last number that was inputted to the inputStorage, so that it can be used in the calculations, and then we reset it
+        inputStorage.push(curInput.join(""));
+        curInput = [];
+
+        // we set acc to be equal to the first number of the stored input, to act as the starting value and then as the result of each operation
+        let acc = Number(inputStorage[0]);
+        
+        // then we iterate through the various operators, and for each we check its value -> after that, we perform the appropriate operation setting the accumulator as the result
+        operatorStorage.forEach((el, curIndex) => {
+            if (el === "+") {
+                acc += Number(inputStorage[curIndex + 1]);
+            } else if (el === "-") {
+                acc -= Number(inputStorage[curIndex + 1]);
+            } else if (el === "*") {
+                acc *= Number(inputStorage[curIndex + 1]);
+            } else if (el === "/") {
+                acc /= Number(inputStorage[curIndex + 1]);
+            }
+        });
+
+        // we store the final result as the first element of the inputStorage array, so we can use it again for the subsequent operations, and we reset the operators
+        inputStorage = [];
+        inputStorage[0] = acc;
+        operatorStorage = [];
+
+        // the result is returned, so that it can be displayed by uiCtrl.displayNumber()
+        return acc;
     }
 
-    function returnNumber() {
-        return numberSwitcher ? tempNumberOne : tempNumberTwo;
+    // curInput is returned, so that it can be displayed by uiCtrl.displayNumber()
+    function returnCurInput() {
+        return curInput.join("");
     }
 
-    function switchNumber() {
-        numberSwitcher = !numberSwitcher;
-    }
-
-    function operation() {
-        if (operator === "+") {
-            tempResult = Number(tempNumberOne) + Number(tempNumberTwo);
-        } else if (operator === "-") {
-            tempResult = Number(tempNumberOne) - Number(tempNumberTwo);
-        } else if (operator === "*") {
-            tempResult = Number(tempNumberOne) * Number(tempNumberTwo);
-        } else if (operator === "/") {
-            tempResult = Number(tempNumberOne) / Number(tempNumberTwo);
+    // the key "c" resets all memory, to start fresh again, while "ce" only erases the last of the curInput values
+    function clearInputs(dset) {
+        if (dset === "c") {
+            curInput = [];
+            inputStorage = [];
+            operatorStorage = [];
+        } else {
+            curInput.pop();
         }
     }
 
-    function storeOperator(dataset) {
-        operator = dataset;
-        console.log(operator);
-    }
-
-    function returnResult() {
-        return tempResult;
-    }
-    
     return {
-        storeInput,
-        resetNumbers,
-        returnNumber,
-        switchNumber,
-        operation,
-        storeOperator,
-        returnResult
+        storeCurInput,
+        operations,
+        returnCurInput,
+        clearInputs
     };
 })();
 
@@ -72,6 +126,7 @@ const uiController = (function() {
                 equals: document.querySelector(".equals")
             },
             digits: {
+                zero: document.getElementById("num-0"),
                 one: document.getElementById("num-1"),
                 two: document.getElementById("num-2"),
                 three: document.getElementById("num-3"),
@@ -85,42 +140,31 @@ const uiController = (function() {
         }
     };
 
-    function displayStoredNumber(num) {
+    function displayNumber(num) {
         domElements.display.textContent = num;
     }
 
     return {
         domElements,
-        displayStoredNumber
+        displayNumber
     };
 })();
 
 const generalController = (function(dataCtrl, uiCtrl) {
     const dom = uiCtrl.domElements;
-
     function setEventListeners() {
         for (let key in dom.keyboard) {
             for (let key2 in dom.keyboard[key]) {
                 dom.keyboard[key][key2].addEventListener("click", function(e) {
                     const dset = dom.keyboard[key][key2].dataset;
-                    if (dset.digit) {
-                        dataCtrl.storeInput(
-                            dom.keyboard[key][key2].dataset.digit
-                        );
-                        uiCtrl.displayStoredNumber(dataCtrl.returnNumber());
-                    } else if (
-                        dset.function === "+" ||
-                        dset.function === "-" ||
-                        dset.function === "*" ||
-                        dset.function === "/"
-                    ) {
-                        dataCtrl.switchNumber();
-                        dataCtrl.storeOperator(dset.function);
-                    } else if (dset.function === "=") {
-                        dataCtrl.operation();
-                        dataCtrl.resetNumbers();
-                        dataCtrl.switchNumber();
-                        uiCtrl.displayStoredNumber(dataCtrl.returnResult());
+                    if (dset.key === "=") {
+                        uiCtrl.displayNumber(dataCtrl.operations());
+                    } else if (dset.key === "ce" || dset.key === "c") {
+                        dataCtrl.clearInputs(dset.key);
+                        uiCtrl.displayNumber(dataCtrl.returnCurInput());
+                    } else {
+                        dataCtrl.storeCurInput(dset.key);
+                        uiCtrl.displayNumber(dataCtrl.returnCurInput());
                     }
                 });
             }
